2ef8ada38d3313baad099a107892f4ff3600fdc6
Flavius <flavius.mester@digital.cabinet-office.gov.uk>
2024-02-29 11:28:42 +0000

    Revert "update sonar config"
    This reverts commit f122e7b989fb866eb014ce47fc24e293d988e1d4.

--- buildLogic/plugins/src/main/kotlin/sonarqube-root-config.gradle.kts
+++ buildLogic/plugins/src/main/kotlin/sonarqube-root-config.gradle.kts
@@ -12,7 +12,7 @@ val packageVersion: String by rootProject.extra
 val rootSonarProperties by rootProject.extra(
     mapOf(
         "sonar.host.url" to System.getProperty("uk.gov.ui.sonar.host.url"),
-        "sonar.token" to System.getProperty("uk.gov.ui.sonar.login"),
+        "sonar.login" to System.getProperty("uk.gov.ui.sonar.login"),
         "sonar.projectKey" to "mobile-android-secure-store",
         "sonar.projectName" to "mobile-android-secure-store",
         "sonar.projectVersion" to packageVersion,
