a59bdac6ec25fcd85db0affea0f376ac189a1182
Oliver BINNS <oliver.binns@digital.cabinet-office.gov.uk>
2024-07-01 20:13:25 +0100

    ci: fix sonarqube upload

--- buildLogic/plugins/src/main/kotlin/uk/gov/securestore/sonarqube-root-config.gradle.kts
+++ buildLogic/plugins/src/main/kotlin/uk/gov/securestore/sonarqube-root-config.gradle.kts
@@ -16,8 +16,8 @@ val rootSonarProperties by rootProject.extra(
     mapOf(
         "sonar.host.url" to "https://sonarcloud.io",
         "sonar.token" to System.getProperty("SONAR_TOKEN"),
-        "sonar.projectKey" to "mobile-android-securestore",
-        "sonar.projectName" to "mobile-android-securestore",
+        "sonar.projectKey" to "mobile-android-secure-store",
+        "sonar.projectName" to "mobile-android-secure-store",
         "sonar.projectVersion" to "${project.versionName}-${project.versionCode}",
         "sonar.organization" to "govuk-one-login",
         "sonar.sourceEncoding" to "UTF-8",
