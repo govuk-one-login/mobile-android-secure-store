d9fcb344c62dac547c483a0d92e3daad3d42b237
Tom Ient <thomas.ient@digital.cabinet-office.gov.uk>
2024-11-08 13:19:41 +0000

    fix: DCMAW-10478 Breaking Pipeline Change (#76)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import uk.gov.pipelines.config.ApkConfig
 buildscript {
     val projectKey: String by rootProject.extra("mobile-android-secure-store")
     val projectId: String by rootProject.extra("uk.gov.securestore")
+    val buildLogicDir: String by rootProject.extra("mobile-android-pipelines/buildLogic")
 
     repositories {
         google()
