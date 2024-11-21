a78aacece153ec4684b6612f5bc72820ff005970
Alex Bradbury <alex.bradbury@digital.cabinet-office.gov.uk>
2024-11-21 11:40:07 +0000

    fix: fix vale issue

--- app/src/main/java/uk/gov/android/securestore/authentication/Authenticator.kt
+++ app/src/main/java/uk/gov/android/securestore/authentication/Authenticator.kt
@@ -16,7 +16,7 @@ interface Authenticator {
      * Start an OS authentication prompt
      *
      * @param accessControlLevel The [AccessControlLevel] required to authenticate for
-     * @param configuration Configuration for the UI elements of the OS Biometric Prompt
+     * @param configuration for the UI elements of the OS Biometric Prompt
      * @param handler An [AuthenticatorCallbackHandler] to handle success, failure and error states from authentication
      */
     fun authenticate(
