6c830a8817fb652c70c1f6385d4e001b38cad0af
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2025-02-26 09:07:50 +0000

    refactor: Rename class starting with R and make it internal
    - increase test coverage as it seems classes starting with R
    are not recignised by jacoco

--- app/src/main/java/uk/gov/android/securestore/crypto/HybridCryptoManagerRsa.kt
+++ app/src/main/java/uk/gov/android/securestore/crypto/HybridCryptoManagerRsa.kt
@@ -19,7 +19,7 @@ import kotlin.io.encoding.ExperimentalEncodingApi
  * be used to encrypt/ decrypt the data provided.
  */
 @OptIn(ExperimentalEncodingApi::class)
-class RsaHybridCryptoManager : HybridCryptoManager {
+internal class HybridCryptoManagerRsa : HybridCryptoManager {
     private lateinit var alias: String
     private lateinit var accessControlLevel: AccessControlLevel
     private val aesCryptoManager = AesCryptoManager()
