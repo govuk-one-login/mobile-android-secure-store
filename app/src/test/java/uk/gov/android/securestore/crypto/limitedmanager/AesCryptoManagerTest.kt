94a84a44f5c1413be068468a771017a3bb400d7b
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-09-03 16:34:53 +0100

    style: Fix line length violation

--- app/src/test/java/uk/gov/android/securestore/crypto/limitedmanager/AesCryptoManagerTest.kt
+++ app/src/test/java/uk/gov/android/securestore/crypto/limitedmanager/AesCryptoManagerTest.kt
@@ -31,11 +31,10 @@ class AesCryptoManagerTest {
         "ZUlhZEVKZ3RhT0M3bjRnUEtrayJ9fX0.Gh0cTcrgh1sX_osWj8SCPQYcQqTNY4D40k_C2Xu3tmSZvePOFU_" +
         "ZT1jA33EvOzzC4pCASiinvXzqVWGoQXWcZA"
     private val invalidKey = Base64.encode(
-        byteArrayOf(
-            -10, 111, -120, 94, -43, -122, -40, 61, 23,
-            12, -83, 34, 4, -96, 50, 15, 86, -35, -43, 65, -39, 116, -128, 119, 124, -3, 103, -5, -87,
-            -58, 39, -124
-        )
+        byteArrayOf(-10, 111, -120, 94, -43, -122, -40, 61, 23) +
+            byteArrayOf(12, -83, 34, 4, -96, 50, 15, 86, -35) +
+            byteArrayOf(-43, 65, -39, 116, -128, 119, 124, -3, 103, -5) +
+            byteArrayOf(-87, -58, 39, -124)
     )
 
     @Test
